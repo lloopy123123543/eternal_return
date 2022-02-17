be7883e912ce7c54f0d371ab18282cff115b3c51
Игорь <baba123123543@gmail.com>
2022-02-17 21:34:32 +0500

    second_com

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -8,7 +8,7 @@ const MainContainer = ({children}) => {
         <div className={styles.header_logo_logo}></div>
         </div>
         <div className={styles.header_menu}>
-        <div className={styles.header_menu_text}><a href='/heroes'>heroes</a></div>
+        <div className={styles.header_menu_text}><a href='/heroes/'>heroes</a></div>
         <div className={styles.header_menu_text}><a href='/'>return</a></div>
         <div className={styles.header_menu_text}>About</div>
         <div className={styles.header_menu_text_button}>
